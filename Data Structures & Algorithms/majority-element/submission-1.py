df4b3214573eaf31499fafb262ae819a34b05ba5
Johnson Addo Ameyaw <jameyaw05@students.livingstone.edu>
2026-04-15 12:59:20 -0400

    Add: majority-element - submission-1

---
+++ Data Structures & Algorithms/majority-element/submission-1.py
@@ -0,0 +1,22 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        n = len(nums)
+        mid = n // 2
+
+        # nums.sort()
+
+        # for i in range(n):
+        #     if nums[i] == nums[i+1]:
+        #         count += 1
+        #         if count 
+        
+        # brute force 
+        # for num in nums:
+        #     count = sum(1 for i in nums if i == num)
+
+        #     if count > n // 2:
+        #         return num
+
+        
+        nums.sort()
+        return nums[mid]
