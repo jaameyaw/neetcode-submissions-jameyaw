d7cd44fe271ff7d148982c759b2295208a28b253
Johnson Addo Ameyaw <jameyaw05@students.livingstone.edu>
2026-05-16 17:27:31 -0400

    Add: longest-consecutive-sequence - submission-1

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-1.py
@@ -0,0 +1,27 @@
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        #array of nums
+        #we need the len of the longest consecutive sequence of elmnts that can be formed
+
+        #what is a consecutive sequence?
+    #basically, each elmt is exactly 1 great than the previous elmet
+    #in other words, their diff is = 1
+
+    #ologn
+    #sort the entire arra
+    #compare n get the diff, if equals 1 add to res
+        if len(nums) == 1:
+            return 1
+            
+        nums.sort()
+
+        res = set()
+#[2,20,4,10,3,4,5]
+#[2,3,4,4,5,10,20]
+        for i in range(1, len(nums)):
+            if nums[i] - nums[i-1] == 1:
+                res.add(nums[i])
+                res.add(nums[i-1])
+        return len(list(res))
+
+#res = [2,3,3,4,4,5]
